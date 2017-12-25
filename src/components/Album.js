8e42c5ef486dc672bfcfbf817018a493f396569d
Isaac Simon <ijsimon@sbcglobal.net>
2017-12-25 16:17:09 -0600

    Checkpoint 6 Assignment

--- src/components/Album.js
+++ src/components/Album.js
@@ -33,6 +33,17 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
+          {this.state.album.songs.map( (song, index) =>
+            <tr className={song} key={index}>
+              <td className="song-actions">
+                  <span className="ion-play"></span>
+                  <span className="ion-pause"></span>
+              </td>
+              <td className="song-number">{index+1 }</td>
+              <td className="song-title">{song.title}</td>
+              <td className="song-duration">{song.duration}</td>
+            </tr>
+          )}
           </tbody>
         </table>
       </section>
